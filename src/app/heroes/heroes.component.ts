39da259c75065ae5016344b5f78f329fa0dc85a0
DynamicDaikon <hietasoba@gmail.com>
2021-11-02 01:01:39 +0900

    comp tutorial no.4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '@app/hero'
 import { HeroService } from '@app/service/hero.service';
+import { MessageService } from '@app/service/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -15,6 +16,7 @@ export class HeroesComponent implements OnInit {
 
   constructor(
     private heroService: HeroService,
+    private messageService: MessageService
   ) { }
 
   public ngOnInit(): void {
@@ -23,6 +25,7 @@ export class HeroesComponent implements OnInit {
 
   public onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesCompornent: Selected hero id=${hero.id}`);
   }
 
   public getHeroes(): void {
